c5a5467184bacd0a7e4e6d6fa25b31b6985a0084
ObjectIsAdvantag <steve.sfartz@gmail.com>
2016-09-14 01:12:03 +0200

    added /about to inspector example

--- examples/inspector.js
+++ examples/inspector.js
@@ -21,12 +21,29 @@ var SparkClient = require("node-sparky");
 var spark = new SparkClient({ token: process.env.SPARK_TOKEN });
 
 
+bot.onCommand("about", function (command) {
+    spark.messageSendRoom(command.message.roomId, {
+        markdown: "```\n{\n   'author':'Stève Sfartz <stsfartz@cisco.com>',\n   'code':'https://github.com/ObjectIsAdvantag/sparkbot-webhook-samples/blob/master/examples/inspector.js',\n   'description':'an handy tool to reveal spark technical data',\n   'healthcheck':'GET https://sparkbot-inspector-objectisadvantag.c9users.io/',\n   'webhook':'POST https://sparkbot-inspector-objectisadvantag.c9users.io/'\n}\n```"
+    });
+});
+
+
+bot.onCommand("fallback", function (command) {
+    // so happy to join
+    spark.messageSendRoom(command.message.roomId, {
+        text: "sorry, I did not understand"
+    })
+        .then(function (message) {
+            // show how to use
+            showHelp(command.message.roomId);
+        });
+});
 bot.onCommand("help", function (command) {
     showHelp(command.message.roomId);
 });
 function showHelp(roomId) {
     spark.messageSendRoom(roomId, {
-        markdown: "I can give you quick access to Spark technical data\n\n- /help\n\n- /room\n\n- /whoami\n\n- /whois @mention\n\n"
+        markdown: "I can give you quick access to Spark technical data:\n- /about\n- /help\n- /room: reveals this room identifier\n- /whoami: shows your spark info\n- /whois @mention: learn about other participants\n"
     });
 }
 
@@ -37,33 +54,6 @@ bot.onCommand("room", function (command) {
     });
 
 });
-
-
-bot.onEvent("memberships", "created", function (trigger) {
-    var newMembership = trigger.data; // see specs here: https://developer.ciscospark.com/endpoint-memberships-get.html
-    if (newMembership.personId == bot.interpreter.person.id) {
-        debug("bot's just added to room: " + trigger.data.roomId);
-
-        // so happy to join
-        spark.messageSendRoom(trigger.data.roomId, {
-            text: "Hi, I am the Inspector Bot !"
-        })
-            .then(function (message) {
-                if (message.roomType == "group") {
-                    spark.messageSendRoom(message.roomId, {
-                        markdown: "**Note that - in Group rooms - I wake up only when mentionned.**"
-                    })
-                        .then(function (message) {
-                            showHelp(message.roomId);
-                        });
-                }
-                else {
-                    showHelp(message.roomId);
-                }
-            });
-    }
-});
-
 
 
 bot.onCommand("whoami", function (command) {
@@ -92,4 +82,28 @@ bot.onCommand("whois", function (command) {
 });
 
 
+bot.onEvent("memberships", "created", function (trigger) {
+    var newMembership = trigger.data; // see specs here: https://developer.ciscospark.com/endpoint-memberships-get.html
+    if (newMembership.personId == bot.interpreter.person.id) {
+        debug("bot's just added to room: " + trigger.data.roomId);
+
+        // so happy to join
+        spark.messageSendRoom(trigger.data.roomId, {
+            text: "Hi, I am the Inspector Bot !"
+        })
+            .then(function (message) {
+                if (message.roomType == "group") {
+                    spark.messageSendRoom(message.roomId, {
+                        markdown: "**Note that this is a 'Group' room. I will wake up only when mentionned.**"
+                    })
+                        .then(function (message) {
+                            showHelp(message.roomId);
+                        });
+                }
+                else {
+                    showHelp(message.roomId);
+                }
+            });
+    }
+});
 
